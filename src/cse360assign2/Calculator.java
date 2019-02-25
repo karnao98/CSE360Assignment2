d4c2cbc9caccbb4a48274cc36e4f1a7bb3499763
keitharnao <keitharnao@keiths-air-5.3521.dhcp.asu.edu>
2019-02-24 18:55:50 -0700

    3. Create the third version

--- src/cse360assign2/Calculator.java
+++ src/cse360assign2/Calculator.java
@@ -1,38 +1,65 @@
+/*
+ * Name: Keith Arnao
+ * Class ID: 318
+ * Assignment 2
+ * Description: Creates Calculator object which can add, subtract, multiply, and divide
+ */
 
 package cse360assign2;
 
+/* 
+ * Calculator.java
+ * Allows for addition, subtraction, multiplication, and division
+ */
 public class Calculator {
 
-	private int total;
+	private int total;	// running total
+	public String history;	// new String concatenated to history each time a mathematical method is called
 	
 	public Calculator () {
-		total = 0;  // not needed - included for clarity
+		total = 0;
+		history = "0";
 	}
 	
+	// Returns current total
 	public int getTotal () {
 		return total;
 	}
 	
+	// Add value to current total
+	// Concatenate operation to history
 	public void add (int value) {
 		total += value;
+		history += " + " + value;
 	}
 	
+	// Subtract value from current total
+	// Concatenate operation to history
 	public void subtract (int value) {
 		total -= value;
+		history += " - " + value;
 	}
 	
+	// Multiply value by current total
+	// Concatenate operation to history
 	public void multiply (int value) {
 		total *= value;
+		history += " * " + value;
 	}
 	
+	// Divide current total by value
+	// Concatenate operation to history
 	public void divide (int value) {
-		if (value == 0)
-			total = 0;
-		else
+		if (value != 0) {
 			total /= value;
+			history += " / " + value;
+		}
+		else
+			total = 0;
 	}
 	
+	// Return history, which is a String of each operation performed
 	public String getHistory () {
-		return "";
+		return (history);
 	}
 }
